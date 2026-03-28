69badfaf96adbf7feb288420d638368399ad0aef
Aishwarya Palani <162413385+Aishwarya5980@users.noreply.github.com>
2026-03-29 00:12:18 +0530

    Implement method to find the third largest number

---
+++ Array/ThirdLargestNumber.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class Main {
+    static int getThirdLargestInt(int[] arr){
+        int largest= Integer.MIN_VALUE;
+        int secondlargest= Integer.MIN_VALUE;
+        int thirdlargest= Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>largest){
+                thirdlargest=secondlargest;
+                secondlargest=largest;
+                largest=arr[i];
+            }else if(arr[i] > secondlargest && arr[i] != largest) {
+                thirdlargest=secondlargest;
+                secondlargest = arr[i];
+            }else if(arr[i] > thirdlargest && arr[i] != largest && arr[i] != secondlargest){
+                thirdlargest= arr[i];
+            }
+            
+        }
+        return thirdlargest;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = { 20,91,42,48 };
+        System.out.println(getThirdLargestInt(arr));
+    }
+}
