962aac004d2e86ae6582270ad279dd3ea3f80ac1
Aishwarya Palani <162413385+Aishwarya5980@users.noreply.github.com>
2026-03-29 00:15:19 +0530

    Implement method to find second largest number

---
+++ Array/SecondLargestNumber.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class SecondLargestNumber {
+    static int getSecondLargestInt(int[] arr){
+        int largest= Integer.MIN_VALUE;
+        int secondlargest=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>largest){
+                secondlargest=largest;
+                largest=arr[i];
+            }else if(arr[i] > secondlargest && arr[i] != largest) {
+                secondlargest = arr[i];
+            }
+        }
+        return secondlargest;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = { 20,91,42,48 };
+        System.out.println(getSecondLargestInt(arr));
+    }
+}
